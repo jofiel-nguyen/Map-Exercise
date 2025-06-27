772b388ba6073009cad3c08642a6f78ea33982e3
Jofiel Nguyen <121688890+jofiel-nguyen@users.noreply.github.com>
2025-06-27 09:28:51 -0500

    Update users.js

--- users.js
+++ users.js
@@ -3,3 +3,13 @@ const users = [
 	{firstName: "Bob", lastName: "Smith", points: 99},
 	{firstName: "Charlie", lastName: "Brown", points: 180}
 ];
+const usersWithStatus = users.map(user => {
+    const fullName = `${user.firstName} ${user.lastName}`;
+    const membershipStatus = user.points > 100 ? "Premium" : "Standard";
+    return {
+        fullName: fullName,
+        membershipStatus: membershipStatus
+    };
+});
+
+console.log(usersWithStatus);
